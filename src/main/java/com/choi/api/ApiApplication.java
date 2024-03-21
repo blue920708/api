f524e7077341b1a39b40dc26ae990ba3a6b8f3b8
blue920708 <58761032+blue920708@users.noreply.github.com>
2024-03-21 15:16:42 +0900

    build war

--- src/main/java/com/choi/api/ApiApplication.java
+++ src/main/java/com/choi/api/ApiApplication.java
@@ -3,6 +3,7 @@ package com.choi.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.lang.annotation.ElementType;
@@ -13,7 +14,7 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.*;
 
 @SpringBootApplication(scanBasePackages = {"com.choi.api"})
-public class ApiApplication {
+public class ApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
